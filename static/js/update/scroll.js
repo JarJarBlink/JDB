5442637393b479379936ab4f286bf9929b7a6d4f
JarJarBlink <34914467+JarJarBlink@users.noreply.github.com>
2026-06-05 10:05:27 +0800

    .

--- static/js/update/scroll.js
+++ static/js/update/scroll.js
@@ -1,12 +1,11 @@
 //Exodia JAR
-$(function (){
 
+$(function (){
 	$(".scl_top").click(function(){
 		$("html,body").stop().animate({scrollTop:0},{duration:800,easing:'swing'});
 		return false;
 	});
 	
-
 	$('#pagetop a').click(function () {
 		$('#pagetop').addClass("move_fly01");
 		$('body,html').stop().animate({scrollTop:0},800,'swing', function(){
@@ -15,33 +14,16 @@ $(function (){
 		
 		return false;
 	});
-	
-	window.addEventListener("load", function(){
-		var page_h = $(document).height();
-		var window_h = $(window).height();
 
-		//console.log("page_h: " + page_h);
-		//console.log("window_h: " + window_h);
-		//console.log("h: " + (page_h - window_h));
-		//console.log("top: " + top);
-		
-		$(window).resize(function(){
-			page_h = $(document).height();
-			window_h = $(window).height();
-		});
+	$(window).scroll(function () {
+		var scrollTop = $(this).scrollTop();
+		var windowH = $(window).height();
+		var docH = $(document).height();
 
-
-		$(window).scroll(function () {
-			page_h = $(document).height(); // 每次捲動時重抓
-			if(!$('#pagetop').attr("class")){
-				if ($(this).scrollTop() >= page_h - window_h -100) {
-					$('#pagetop').addClass("move_up01");
-				}
+		if (!$('#pagetop').attr("class")) {
+			if (scrollTop + windowH >= docH - 200) {
+				$('#pagetop').addClass("move_up01");
 			}
-			
-		});
-
-	});//addEventListener("load", function()
-	
-	
+		}
+	});
 });
